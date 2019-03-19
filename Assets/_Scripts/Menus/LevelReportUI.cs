d8f5def4c17d4b9b48d9b7c44fb18a90c7a81316
Alejandro Santiago <>
2019-03-19 12:38:57 +0100

    Afegir escena LevelReport

---
+++ Assets/_Scripts/Menus/LevelReportUI.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace ioc.IOCStudents.Menus
+{
+    public class LevelReportUI : MonoBehaviour
+    {
+        [SerializeField]
+        protected TextMeshProUGUI m_ScoreText = null;
+
+        [SerializeField]
+        protected TextMeshProUGUI m_enmiesKilledText = null;
+
+        [SerializeField]
+        protected TextMeshProUGUI m_PlayedTime = null;
+
+
+        protected void Start()
+        {
+            m_ScoreText.text = "0000";
+            m_enmiesKilledText.text = "0";
+            m_PlayedTime.text = "0m 0s";
+        }
+    }
+}
---
+++ Assets/_Scripts/SceneController/LevelReportController.cs
@@ -0,0 +1,31 @@
+﻿using ioc.IOCStudents.Core;
+using UnityEngine;
+
+namespace ioc.IOCStudents.SceneController
+{
+    public class LevelReportController : MonoBehaviour
+    {
+        private bool m_isActive;
+
+        protected void Start()
+        {
+            m_isActive = true;
+        }
+
+        protected void Update()
+        {
+            if (!m_isActive)
+            {
+                return;
+            }
+
+            if (InputManager.Instance.JumpButton.State == IMButton.ButtonStates.ButtonDown
+                || InputManager.Instance.ShootButton.State == IMButton.ButtonStates.ButtonDown)
+            {
+                GameManager.Instance.ToManinMenu();
+                m_isActive = false;
+            }
+
+        }
+    }
+}
