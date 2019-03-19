7bc37d68981b0b01415b6c2a2c1f0c0bc1d47d00
Alejandro Santiago <>
2019-03-19 12:37:01 +0100

    Menu principal (video 5)

---
+++ Assets/_Scripts/SceneController/MainMenuController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using ioc.IOCStudents.Core;
+
+namespace ioc.IOCStudents.SceneController
+{
+    public class MainMenuController : MonoBehaviour
+    {
+        [SerializeField]
+        protected RectTransform m_fadeButtonFrame = null;
+
+        protected void Start()
+        {
+            InputManager.Instance.JumpButton.m_buttonDownMethod += OnStartLevelPressed;
+
+            LeanTween.alpha(m_fadeButtonFrame, 0.0f, 2.0f).setLoopPingPong();
+        }
+
+        private void OnStartLevelPressed()
+        {
+            LeanTween.cancel(m_fadeButtonFrame);
+            m_fadeButtonFrame.gameObject.SetActive(false);
+
+            //GameManager.Instance.ToGame();
+            GameManager.Instance.ToTransitionScene2();
+        }
+
+        protected void OnDisable()
+        {
+            if (InputManager.Instance.JumpButton == null)
+            {
+                return;
+            }
+
+            InputManager.Instance.JumpButton.m_buttonDownMethod -= OnStartLevelPressed;
+        }
+    }
+}
