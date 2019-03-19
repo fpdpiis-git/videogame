57dafe041f8b32990a922ca3c90474a70e8919d6
Alejandro Santiago <>
2019-03-19 12:35:39 +0100

    Scene de Splash (video 4)

---
+++ Assets/_Scripts/SceneController/SplashController.cs
@@ -0,0 +1,35 @@
+﻿using ioc.IOCStudents.Core;
+using System.Collections;
+using UnityEngine;
+
+namespace ioc.IOCStudents.SceneController
+{
+    public class SplashController : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject m_logo = null;
+
+        
+        protected void Start()
+        {
+            LeanTween.alpha(m_logo, 1.0f, 2.0f).setOnComplete(OnAlphaInEnded);
+        }
+
+        private void OnAlphaInEnded()
+        {
+            StartCoroutine(OnWait());
+        }
+
+        private IEnumerator OnWait()
+        {
+            yield return new WaitForSeconds(2.0f);
+            LeanTween.alpha(m_logo, 0.0f, 2.0f).setOnComplete(OnAlphaOutEnded);
+        }
+
+        private void OnAlphaOutEnded()
+        {
+            // Aqui aniara la transicio al menu principal
+            GameManager.Instance.ToTransitionScene2();
+        }        
+    }
+}
